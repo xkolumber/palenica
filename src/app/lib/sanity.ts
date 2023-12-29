d1f7a67b59f1baf9cc5fc59f849c848b6d91ebbf
xkolumber <lubosk158@gmail.com>
2023-12-30 00:59:46 +0100

    Basic article page

---
+++ src/app/lib/sanity.ts
@@ -0,0 +1,12 @@
+import { createClient } from "@sanity/client";
+
+const projectId = "q10qjbs8";
+const dataset = "production";
+const apiVersion = "2023-01-01";
+
+export const client = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: true,
+});
